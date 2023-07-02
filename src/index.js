1c99251e2a4f0a10abc3fb6ec78f8f2f0223044a
pier <pierknows@gmail.com>
2023-07-02 18:44:15 -0400

    fixed API with static class

--- src/index.js
+++ src/index.js
@@ -1,36 +1,36 @@
-// import fetch from "node-fetch";
-// import './css/fonts.css';
-// import './css/styles.css';
+// // import fetch from "node-fetch";
+// // import './css/fonts.css';
+// // import './css/styles.css';
 
-// globalThis.fetch = fetch;
+// // globalThis.fetch = fetch;
 
-// const url = 'https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD';
-// https://v6.exchangerate-api.com/v6/fe85d98e3455a344155098c6/latest/USD
-
-
-// const result = document.querySelector('.result');
-
-async function fetchData() {
-    const url = "https://v6.exchangerate-api.com/v6/fe85d98e3455a344155098c/pair/EUR/GBP";
-
-    try {
-
-        const response = await fetch(url);
-        const data = await response.json();
-        return data;
-    } catch (err) {
-        console.error(err);
-        throw err;
-    }
-
-}
+// // const url = 'https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD';
+// // https://v6.exchangerate-api.com/v6/fe85d98e3455a344155098c6/latest/USD
 
 
-// console.log(fetchData());
-export default fetchData;
+// // const result = document.querySelector('.result');
+
+// async function fetchData() {
+//     const url = "https://v6.exchangerate-api.com/v6/fe85d98e3455a344155098c/pair/EUR/GBP";
+
+//     try {
+
+//         const response = await fetch(url);
+//         const data = await response.json();
+//         return data;
+//     } catch (err) {
+    
+//         throw err;
+//     }
+
+// }
 
 
-// node-fetch
-// npm install node-fetch
+// // console.log(fetchData());
+// export default fetchData;
 
-//https://www.learnhowtoprogram.com/intermediate-javascript
+
+// // node-fetch
+// // npm install node-fetch
+
+// //https://www.learnhowtoprogram.com/intermediate-javascript
