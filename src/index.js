be4b5d1dfd17a0dd14650e19c4647d04318e3ec0
pier <pierknows@gmail.com>
2023-07-10 11:46:41 -0400

    updated file after fixing HEAD whatever

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import apiCurrency from "./js/scripts/apiCurrency";
 
-async function fetchConversion(from, to) {
-  try {
+async function getConversion(from, to) {
     const data = await apiCurrency.getConversion(from, to);
-    return data.conversion_rate;
-  } catch (error) {
-    console.error(error);
+    if (data.result){
+      printElements(data, from, to);
+    } else {
+      printError(data, from, to);
   }
 }
 
-//ui logic
+
+function printElements(data, from, to){
+  document.querySelector("show-response").innerText =`${to} is ${data.conversion_result} in ${from}`;
+}
+
+function printError(error){
+  document.querySelector("show-response").innerText =`The currency is not a real currency! ${error}`;
+}
+
 
 const cForm = document.getElementById("conversion-form");
 
@@ -21,59 +29,6 @@ cForm.addEventListener("submit", async (event) => {
   const toCurrency = document.getElementById("to-currency").value;
   const amount = document.getElementById("amount").value;
   console.log({ fromCurrency, toCurrency, amount });
-  const rate = await fetchConversion(fromCurrency, toCurrency);
+  const rate = await getConversion(fromCurrency, toCurrency);
   result.innerHTML = rate * parseInt(amount);
 });
-
-// function fetchConversion() {
-//   apiCurrency.getConversion()
-//     .then(data => {
-//       console.log(data);
-//     })
-//     .catch(error => {
-//       console.log(error);
-//     });
-// }
-
-// document.addEventListener('DOMContentLoaded', () => {
-//   const form = document.querySelector('form');
-
-//   form.addEventListener('submit', event => {
-//     event.preventDefault();
-//     fetchConversion();
-//   });
-// });
-
-// // import fetch from "node-fetch";
-// // import './css/fonts.css';
-// // import './css/styles.css';
-
-// // globalThis.fetch = fetch;
-
-// // const url = 'https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD';
-// // https://v6.exchangerate-api.com/v6/fe85d98e3455a344155098c6/latest/USD
-
-// // const result = document.querySelector('.result');
-
-// async function fetchData() {
-//     const url = "https://v6.exchangerate-api.com/v6/fe85d98e3455a344155098c/pair/EUR/GBP";
-
-//     try {
-
-//         const response = await fetch(url);
-//         const data = await response.json();
-//         return data;
-//     } catch (err) {
-
-//         throw err;
-//     }
-
-// }
-
-// // console.log(fetchData());
-// export default fetchData;
-
-// // node-fetch
-// // npm install node-fetch
-
-// //https://www.learnhowtoprogram.com/intermediate-javascript
--- src/js/scripts/apiCurrency.js
+++ src/js/scripts/apiCurrency.js
@@ -1,19 +1,15 @@
 export default class apiCurrency {
   static async getConversion(from, to) {
     try {
-      const response = await fetch(
-        `https://v6.exchangerate-api.com/v6/fe85d98e3455a344155098c6/pair/${from}/${to}`
-      );
-
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/fe85d98e3455a344155098c6/pair/${from}/${to}/`);
+      const json = await response.json();
       if (!response.ok) {
-        const errorMessage = `${response.status} ${response.statusText}`;
+        const errorMessage = `${response.status} ${json['error-type']}`;
         throw new Error(errorMessage);
       }
-
-      return response.json();
-    } catch (error) {
-      console.log("Invalid_key");
+      return json;
+    } catch(error) {
       return error;
     }
+    }
   }
-}
