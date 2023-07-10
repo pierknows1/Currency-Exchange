ffceb820c8396274797d39794c4498b2b33883e3
pier <pierknows@gmail.com>
2023-07-10 17:20:29 -0400

    for some reason im seeing 2 bugs on this version, will quickly update

--- src/js/scripts/apiCurrency.js
+++ src/js/scripts/apiCurrency.js
@@ -1,7 +1,7 @@
 export default class apiCurrency {
   static async getConversion(from, to) {
       try {
-          const response = await fetch(`https://v6.exchangerate-api.com/v6/fe85d98e3455a344155098c6/pair/${from}/${to}/`);
+          const response = await fetch(`https://v6.exchangerate-api.com/v6/{process.env.API_KEY/pair}/${from}/${to}/`);
           const json = await response.json();
           if (!response.ok) {
               const errorMessage = `${response.status} ${json['error-type']}`;
