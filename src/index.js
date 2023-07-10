21fe17421b28065c47213c86597ca294c1986e2a
pier <pierknows@gmail.com>
2023-07-10 17:52:46 -0400

    EVERYTHING  WORKSSSSS, but for the error choose the last currencies

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import apiCurrency from "./js/scripts/apiCurrency";
-const showResponse = document.getElementById("show-response");
-console.log({ showResponse });
+
 async function getConversion(from, to, amount) {
-  const data = await apiCurrency.getConversion(from, to);
-  const conversionValue = data.conversion_rate * amount;
-  if (data.result) {
-    printElements(conversionValue, from, to);
-  } else {
-    printError(data.message);
+  try {
+    const data = await apiCurrency.getConversion(from, to);
+    const conversionValue = data.conversion_rate * amount;
+    
+    if (data.result) {
+      printElements(conversionValue, from, to);
+    } else {
+      printError(data.message);
+    }
+  } catch (error) {
+    printError(error);
   }
 }
+
+const showResponse = document.getElementById("show-response");
 
 function printElements(data, from, to) {
   console.log({ data });
@@ -21,15 +27,14 @@ function printError(error) {
 }
 
 const cForm = document.getElementById("conversion-form");
-
-const result = document.getElementById("result");
-
 cForm.addEventListener("submit", async (event) => {
   event.preventDefault();
+  
   const fromCurrency = document.getElementById("from-currency").value;
   const toCurrency = document.getElementById("to-currency").value;
   const amount = document.getElementById("amount").value;
+  
   console.log({ fromCurrency, toCurrency, amount });
-  const rate = await getConversion(fromCurrency, toCurrency, amount);
-  //   result.innerHTML = rate * parseInt(amount);
+  await getConversion(fromCurrency, toCurrency, amount);
+  
 });
